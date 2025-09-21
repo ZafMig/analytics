f9acacd49a9d9ca51d707d74b6e7ac33076219ae
Zaf_Mig <interlait3n90@gmail.com>
2025-09-21 09:47:34 +0300

    version 1.2

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000
-  },
+  base: './',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
